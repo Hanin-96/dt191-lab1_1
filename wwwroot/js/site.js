04001be85a47694534bafe624b968d1c24d11e02
Hanin Farhan <haninfarhan96@gmail.com>
2025-01-23 17:03:21 +0100

    Lagt till js funktionalitet för att sätta nav länk klass active

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Lägger till active klass på aktiv nav element för att få svart bakgrund
+document.addEventListener("DOMContentLoaded", () => {
+    let allNavLinks = document.querySelectorAll(".nav-item");
+    allNavLinks.forEach((navLink) => {
+        if (navLink.getAttribute("href").toLowerCase() === location.pathname.toLowerCase()) {
+            navLink.classList.add("active")
+        } else {
+            navLink.classList.remove("active");
+        }
+    });
+})
