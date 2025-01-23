7f3bdb7bb6f255682275805e05dbadb6cf412f61
Hanin Farhan <haninfarhan96@gmail.com>
2025-01-23 11:19:26 +0100

    Fixat nav länkar med specifika sidor

---
+++ Pages/RazorPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab1_1.Pages
+{
+    public class RazorPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
