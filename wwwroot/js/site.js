db3016d1ecb4169adfad5383f6f5950900cb2f5d
Hanin Farhan <haninfarhan96@gmail.com>
2025-01-29 10:41:21 +0100

    kommentarer + css styling

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,15 +1,15 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿"use strict";
 
 //Lägger till active klass på aktiv nav element för att få svart bakgrund
 document.addEventListener("DOMContentLoaded", () => {
     let allNavLinks = document.querySelectorAll(".nav-item");
+    //Loopar igenom alla nav-länkar
     allNavLinks.forEach((navLink) => {
+        //Kontrollerar om länken är den valda path och lägger till klassen active
         if (navLink.getAttribute("href").toLowerCase() === location.pathname.toLowerCase()) {
             navLink.classList.add("active")
         } else {
+            //Ta bort klassen active om nav-länken inte stämmer överens med path
             navLink.classList.remove("active");
         }
     });
